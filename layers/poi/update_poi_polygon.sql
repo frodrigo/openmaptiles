f4af10c896f28be95263169b990702ebbc00bf4a
Frédéric Rodrigo <fred.rodrigo@gmail.com>
2020-07-23 12:20:05 +0200

    Make osm_poi_polygon use incremental update #814

--- layers/poi/update_poi_polygon.sql
+++ layers/poi/update_poi_polygon.sql
@@ -1,9 +1,8 @@
-DROP TRIGGER IF EXISTS trigger_flag ON osm_poi_polygon;
-DROP TRIGGER IF EXISTS trigger_refresh ON poi_polygon.updates;
+DROP TRIGGER IF EXISTS trigger_update_polygon ON osm_poi_polygon;
 
 -- etldoc:  osm_poi_polygon ->  osm_poi_polygon
 
-CREATE OR REPLACE FUNCTION update_poi_polygon() RETURNS void AS
+CREATE OR REPLACE FUNCTION update_poi_polygon(new_osm_id bigint) RETURNS void AS
 $$
 BEGIN
     UPDATE osm_poi_polygon
@@ -13,66 +12,47 @@ BEGIN
                     THEN ST_Centroid(geometry)
                 ELSE ST_PointOnSurface(geometry)
                 END
-    WHERE ST_GeometryType(geometry) <> 'ST_Point';
+    WHERE (new_osm_id IS NULL OR osm_id = new_osm_id)
+      AND ST_GeometryType(geometry) <> 'ST_Point';
 
     UPDATE osm_poi_polygon
     SET subclass = 'subway'
-    WHERE station = 'subway'
+    WHERE (new_osm_id IS NULL OR osm_id = new_osm_id)
+      AND station = 'subway'
       AND subclass = 'station';
 
     UPDATE osm_poi_polygon
     SET subclass = 'halt'
-    WHERE funicular = 'yes'
+    WHERE (new_osm_id IS NULL OR osm_id = new_osm_id)
+      AND funicular = 'yes'
       AND subclass = 'station';
 
     UPDATE osm_poi_polygon
     SET tags = update_tags(tags, geometry)
-    WHERE COALESCE(tags->'name:latin', tags->'name:nonlatin', tags->'name_int') IS NULL;
+    WHERE (new_osm_id IS NULL OR osm_id = new_osm_id)
+      AND COALESCE(tags->'name:latin', tags->'name:nonlatin', tags->'name_int') IS NULL
+      AND tags != update_tags(tags, geometry);
 
-    ANALYZE osm_poi_polygon;
 END;
 $$ LANGUAGE plpgsql;
 
-SELECT update_poi_polygon();
+SELECT update_poi_polygon(NULL);
 
 -- Handle updates
 
 CREATE SCHEMA IF NOT EXISTS poi_polygon;
 
-CREATE TABLE IF NOT EXISTS poi_polygon.updates
-(
-    id serial PRIMARY KEY,
-    t text,
-    UNIQUE (t)
-);
-CREATE OR REPLACE FUNCTION poi_polygon.flag() RETURNS trigger AS
+CREATE OR REPLACE FUNCTION poi_polygon.update() RETURNS trigger AS
 $$
 BEGIN
-    INSERT INTO poi_polygon.updates(t) VALUES ('y') ON CONFLICT(t) DO NOTHING;
+    PERFORM update_poi_polygon(NEW.osm_id);
     RETURN NULL;
 END;
 $$ LANGUAGE plpgsql;
 
-CREATE OR REPLACE FUNCTION poi_polygon.refresh() RETURNS trigger AS
-$$
-BEGIN
-    RAISE LOG 'Refresh poi_polygon';
-    PERFORM update_poi_polygon();
-    -- noinspection SqlWithoutWhere
-    DELETE FROM poi_polygon.updates;
-    RETURN NULL;
-END;
-$$ LANGUAGE plpgsql;
-
-CREATE TRIGGER trigger_flag
-    AFTER INSERT OR UPDATE OR DELETE
+CREATE CONSTRAINT TRIGGER trigger_update_polygon
+    AFTER INSERT OR UPDATE
     ON osm_poi_polygon
-    FOR EACH STATEMENT
-EXECUTE PROCEDURE poi_polygon.flag();
-
-CREATE CONSTRAINT TRIGGER trigger_refresh
-    AFTER INSERT
-    ON poi_polygon.updates
     INITIALLY DEFERRED
     FOR EACH ROW
-EXECUTE PROCEDURE poi_polygon.refresh();
+EXECUTE PROCEDURE poi_polygon.update();
